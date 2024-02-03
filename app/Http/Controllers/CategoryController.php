c763b46e7f1e5174914029234f1fffb7d56d49eb
SenghongVat <vatsenghong@gmail.com>
2024-02-03 23:56:22 +0700

    tp05

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    //Get /api/categories
+    public function getCategories(){}
+
+    //Post /api/categories
+    public function createCategories(){}
+
+    //Get /api/categories/{categoryID}
+    public function getCategory($categoryID){}
+
+    //Patch /api/categories/{categoryID}
+    public function updateCategory($categoryID){}
+
+    //Delete /api/categories/{categoryID}
+    public function deleteCategory($categoryID){}
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome Page</title>
+
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+
+        <style>
+            .container{
+                margin: 50px auto;
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="container">
+            <h3>Welcome to Ecommerce backend server.</h3>
+            <h5>Powered by Laravel v.10 <br> Version 0.1</h5>
+        </div>
+    </body>
+</html>
